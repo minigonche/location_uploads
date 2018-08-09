ea3ef3b7d2633aef549df5b4816a10b5d3cc9c51
Felipe Gonzalez Casabianca <f.gonzalez1899@uniandes.edu.co>
2018-08-08 22:12:12 -0500

    more

--- website/publisher.py
+++ website/publisher.py
@@ -32,26 +32,26 @@ while(True):
 
 	try:
 
-	file_names = os.listdir(json_location)
-	np.random.shuffle(file_names)
+		file_names = os.listdir(json_location)
+		np.random.shuffle(file_names)
 
-	if(len(file_names) == 0):
-		write_to_log('Nothing found in folder. Sleeping for: ' + str(int(sleep_time/60)) + ' minutes')
-		time.sleep(sleep_time)
-	else:
-		for file_name in file_names:
+		if(len(file_names) == 0):
+			write_to_log('Nothing found in folder. Sleeping for: ' + str(int(sleep_time/60)) + ' minutes')
+			time.sleep(sleep_time)
+		else:
+			for file_name in file_names:
 
-			if not file_name.endswith('.json'):
-				write_to_log('Found: ' + file_name + ' which is not a JSON. Deleting file...')
-				os.remove(location + file_name)
-			else:
-				json_file = open(location + file_name, 'r')
-				data = json.load(json_file)
+				if not file_name.endswith('.json'):
+					write_to_log('Found: ' + file_name + ' which is not a JSON. Deleting file...')
+					os.remove(location + file_name)
+				else:
+					json_file = open(location + file_name, 'r')
+					data = json.load(json_file)
 
-				write_to_log('Exporting: ' + file_name )
-				rows = ps.export_json(data['interview_id'], data)
-				write_to_log( 'Done. Added: ' + str(rows) + ' records to database. Deleting file...')
-				os.remove(location + file_name)
+					write_to_log('Exporting: ' + file_name )
+					rows = ps.export_json(data['interview_id'], data)
+					write_to_log( 'Done. Added: ' + str(rows) + ' records to database. Deleting file...')
+					os.remove(location + file_name)
 
 	except Exception as e:
 	    write_to_log('-----------------')
