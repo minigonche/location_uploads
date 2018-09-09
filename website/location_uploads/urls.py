fb598246c291616d2072c3c6fa4a0dddd78e9f2d
Felipe Gonzalez Casabianca <f.gonzalez1899@uniandes.edu.co>
2018-09-09 10:06:03 -0500

    MOre changes for profuction

--- website/location_uploads/urls.py
+++ website/location_uploads/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
@@ -11,4 +13,4 @@ urlpatterns = [
     path('terms', views.terms, name='terms'),
     path('upload_survey', views.upload_survey, name='upload_survey'),
     path('upload_json', views.upload_json, name='upload_json')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
